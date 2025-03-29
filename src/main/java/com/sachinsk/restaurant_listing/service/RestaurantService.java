6d32b0622175abdc9336a729144907c6e7aae585
Sachin <sachinkaushikofficial@gmail.com>
2025-03-29 13:36:26 +0530

    Adding mapping logic to restaurant service class

--- src/main/java/com/sachinsk/restaurant_listing/service/RestaurantService.java
+++ src/main/java/com/sachinsk/restaurant_listing/service/RestaurantService.java
@@ -2,11 +2,13 @@ package com.sachinsk.restaurant_listing.service;
 
 import com.sachinsk.restaurant_listing.dto.RestaurantDTO;
 import com.sachinsk.restaurant_listing.entity.Restaurant;
+import com.sachinsk.restaurant_listing.mapper.RestaurantMapper;
 import com.sachinsk.restaurant_listing.repository.RestaurantRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class RestaurantService {
@@ -16,8 +18,12 @@ public class RestaurantService {
 
     public List<RestaurantDTO> findAllRestaurants() {
         List<Restaurant> restaurants = restaurantRepo.findAll();
-        //map the entities fetched above to the List of DTOs
 
-        return null;
+        //map the entities fetched above to the List of DTOs
+        List<RestaurantDTO> restaurantDTOList = restaurants.stream()
+                .map(restaurant -> RestaurantMapper.INSTANCE.mapRestaurantToRestaurantDTO(restaurant))
+                .collect(Collectors.toList());
+
+        return restaurantDTOList;
     }
 }
