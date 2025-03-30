8987f9a8599382f1e6c4a37d56d435621fccff33
Sachin <sachinkaushikofficial@gmail.com>
2025-03-30 23:25:34 +0530

    Removing unnecessary imports and annotation

--- src/main/java/com/sachinsk/restaurantlisting/service/RestaurantService.java
+++ src/main/java/com/sachinsk/restaurantlisting/service/RestaurantService.java
@@ -1,17 +1,9 @@
 package com.sachinsk.restaurantlisting.service;
 
 import com.sachinsk.restaurantlisting.dto.RestaurantDTO;
-import com.sachinsk.restaurantlisting.entity.Restaurant;
-import com.sachinsk.restaurantlisting.mapper.RestaurantMapper;
-import com.sachinsk.restaurantlisting.repository.RestaurantRepo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
-@Service
 public interface RestaurantService {
 
     public List<RestaurantDTO> findAllRestaurants();
